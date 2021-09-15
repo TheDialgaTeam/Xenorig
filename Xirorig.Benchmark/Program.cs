5c1bc020b2df5e1e7d6cea2d2b50fbd66a3a3308
Yong Jian Ming <jianming1993@gmail.com>
2021-09-15 21:47:37 +0800

    No benchmark...

--- Xirorig.Benchmark/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using BenchmarkDotNet.Running;
-
-namespace Xirorig.Benchmark
-{
-    internal class Program
-    {
-        public static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
-    }
-}
