822165b50b8eeed20b6dcf2c2dbf9def3eee1ef4
Yong Jian Ming <jianming1993@gmail.com>
2021-09-17 03:08:58 +0800

    Update CI

---
+++ appveyor/build_script.ps1
@@ -0,0 +1,88 @@
+# XIRORIG_NATIVE_ROOT=Xirorig.Native/xirorig_native
+
+if (${env:APPVEYOR_JOB_NAME} -eq "Build Xirorig_Native Windows") {
+    # Build Xirorig Native x64
+    $env:CHERE_INVOKING = 'yes'
+    $env:MSYSTEM = 'MINGW64'
+
+    C:\msys64\usr\bin\bash -lc "cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/install/x64-windows -DVCPKG_TARGET_TRIPLET=x64-mingw-static -S ${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT} -B ${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/build/x64-windows && ninja -C ${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/build/x64-windows install"
+
+    # Build Xirorig Native x86
+    $env:MSYSTEM = 'MINGW32'
+
+    C:\msys64\usr\bin\bash -lc "cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/install/x86-windows -DVCPKG_TARGET_TRIPLET=x86-mingw-static -S ${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT} -B ${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/build/x86-windows && ninja -C ${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/build/x86-windows install"
+
+    # Build Xirorig Native ARM32
+    cmd /c "call 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsamd64_arm.bat' && cmake -G 'Visual Studio 16 2019' -A ARM -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/install/ARM32-windows -DVCPKG_TARGET_TRIPLET=arm-windows-static -DEXCLUDE_CPU_INFO_DEPENDENCY=true -S ${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT} -B ${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/build/ARM32-windows && msbuild.exe ${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/build/ARM32-windows/INSTALL.vcxproj -p:Configuration=Release"
+
+    # Build Xirorig Native ARM64
+    cmd /c "call 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsamd64_arm64.bat' && cmake -G 'Visual Studio 16 2019' -A ARM64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/install/ARM64-windows -DVCPKG_TARGET_TRIPLET=arm64-windows-static -DEXCLUDE_CPU_INFO_DEPENDENCY=true -S ${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT} -B ${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/build/ARM64-windows && msbuild.exe ${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/build/ARM64-windows/INSTALL.vcxproj -p:Configuration=Release"
+
+    Set-Location "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/install"
+
+    7z a ${env:XIRORIG_NATIVE_WINDOWS_ARTIFACT_NAME} * 
+} elseif (${env:APPVEYOR_JOB_NAME} -eq "Build Xirorig_Native Linux") {
+    # Build Xirorig Native x64
+    cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/install/x64-linux" -DVCPKG_TARGET_TRIPLET=x64-linux -S "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}" -B "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/build/x64-linux"
+    ninja -C "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/build/x64-linux" install
+
+    # Build Xirorig Native ARM32
+    cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/install/ARM32-linux" -DVCPKG_TARGET_TRIPLET=arm-linux -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE="${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/cmake/raspberrypi-arm.cmake" -S "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}" -B "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/build/ARM32-linux"
+    ninja -C "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/build/ARM32-linux" install
+
+    # Build Xirorig Native ARM64
+    cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/install/ARM64-linux" -DVCPKG_TARGET_TRIPLET=arm64-linux -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE="${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/cmake/raspberrypi-arm64.cmake" -S "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}" -B "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/build/ARM64-linux"
+    ninja -C "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/build/ARM64-linux" install
+
+    Set-Location "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/install"
+
+    7z a ${env:XIRORIG_NATIVE_LINUX_ARTIFACT_NAME} * 
+} elseif (${env:APPVEYOR_JOB_NAME} -eq "Build Xirorig_Native MacOS") {
+    # Build Xirorig Native x64
+    cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/install/x64-osx" -DVCPKG_TARGET_TRIPLET=x64-osx -S "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}" -B "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/build/x64-osx"
+    ninja -C "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/build/x64-osx" install
+
+    Set-Location "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/install"
+
+    7z a ${env:XIRORIG_NATIVE_MACOS_ARTIFACT_NAME} * 
+} elseif (${env:APPVEYOR_JOB_NAME} -eq "Build Xirorig Windows") {
+    $dotnet = "${env:APPVEYOR_BUILD_FOLDER}/../dotnet"
+
+    "$dotnet publish -p:PublishProfile=x64-windows.pubxml -p:Platform=x64 -c Release_windows"
+    "$dotnet publish -p:PublishProfile=x86-windows.pubxml -p:Platform=x86 -c Release_windows"
+    "$dotnet publish -p:PublishProfile=arm-windows.pubxml -p:Platform=ARM32 -c Release_windows"
+    "$dotnet publish -p:PublishProfile=arm64-windows.pubxml -p:Platform=ARM64 -c Release_windows"
+
+    Set-Location Xirorig/publish
+
+    7z a x64-windows.zip x64-windows -mx=9
+    7z a x86-windows.zip x86-windows -mx=9
+    7z a arm-windows.zip arm-windows -mx=9
+    7z a arm64-windows.zip arm64-windows -mx=9
+} elseif (${env:APPVEYOR_JOB_NAME} -eq "Build Xirorig Linux") {
+    $dotnet = "${env:APPVEYOR_BUILD_FOLDER}/../dotnet"
+
+    "$dotnet publish -p:PublishProfile=x64-linux.pubxml -p:Platform=x64 -c Release_linux"
+    "$dotnet publish -p:PublishProfile=arm-linux.pubxml -p:Platform=ARM32 -c Release_linux"
+    "$dotnet publish -p:PublishProfile=arm64-linux.pubxml -p:Platform=ARM64 -c Release_linux"
+
+    Set-Location Xirorig/publish
+
+    7z a x64-linux.tar x64-linux
+    7z a x64-linux.tar.gz x64-linux.tar -mx=9
+
+    7z a arm-linux.tar arm-linux
+    7z a arm-linux.tar.gz arm-linux.tar -mx=9
+
+    7z a arm64-linux.tar arm64-linux
+    7z a arm64-linux.tar.gz arm64-linux.tar -mx=9
+} elseif (${env:APPVEYOR_JOB_NAME} -eq "Build Xirorig MacOS") {
+    $dotnet = "${env:APPVEYOR_BUILD_FOLDER}/../dotnet"
+
+    "$dotnet publish -p:PublishProfile=x64-osx.pubxml -p:Platform=x64 -c Release_osx"
+
+    Set-Location Xirorig/publish
+
+    7z a x64-osx.tar x64-osx
+    7z a x64-osx.tar.gz x64-osx.tar -mx=9
+}
---
+++ appveyor/install.ps1
@@ -0,0 +1,167 @@
+# APPVEYOR_TOKEN_KEY
+# XIRORIG_NATIVE_WINDOWS_ARTIFACT_NAME
+# XIRORIG_NATIVE_LINUX_ARTIFACT_NAME
+# XIRORIG_NATIVE_MACOS_ARTIFACT_NAME
+
+function Get-AppVeyorArtifacts
+{
+    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Low')]
+    param(
+        #The name of the account you wish to download artifacts from
+        [parameter(Mandatory = $true)]
+        [string]$Account,
+        #The name of the project you wish to download artifacts from
+        [parameter(Mandatory = $true)]
+        [string]$Project,
+        #Where to save the downloaded artifacts. Defaults to current directory.
+        [alias("DownloadDirectory")][string]$Path = '.',
+        [string]$Token,
+        #Filter to a specific branch or project directory. You can specify Branch as either branch name ("master") or build version ("0.1.29")
+        [string]$Branch,
+        #If you have multiple build jobs, specify which job you wish to retrieve the artifacts from
+        [string]$JobName,
+        #Download all files into a single directory, do not preserve any hierarchy that might exist in the artifacts
+        [switch]$Flat,
+        [string]$Proxy,
+        [switch]$ProxyUseDefaultCredentials,
+        #URL of Appveyor API. You normally shouldn't need to change this.
+        $apiUrl = 'https://ci.appveyor.com/api'
+    )
+
+    $headers = @{
+        'Content-type' = 'application/json'
+    }
+
+    if ($Token) {$headers.'Authorization' = "Bearer $token"}
+
+    # Prepare proxy args to splat to Invoke-RestMethod
+    $proxyArgs = @{}
+    if (-not [string]::IsNullOrEmpty($proxy)) {
+        $proxyArgs.Add('Proxy', $proxy)
+    }
+    if ($proxyUseDefaultCredentials.IsPresent) {
+        $proxyArgs.Add('ProxyUseDefaultCredentials', $proxyUseDefaultCredentials)
+    }
+
+    $errorActionPreference = 'Stop'
+    $projectURI = "$apiUrl/projects/$account/$project"
+    if ($Branch) {$projectURI = $projectURI + "/branch/$Branch"}
+
+    $projectObject = Invoke-RestMethod -Method Get -Uri $projectURI `
+                                       -Headers $headers @proxyArgs
+
+    if (-not $projectObject.build.jobs) {throw "No jobs found for this project or the project and/or account name was incorrectly specified"}
+
+    if (($projectObject.build.jobs.count -gt 1) -and -not $jobName) {
+        throw "Multiple Jobs found for the latest build. Please specify the -JobName paramter to select which job you want the artifacts for"
+    }
+
+    if ($JobName) {
+        $jobid = ($projectObject.build.jobs | Where-Object name -eq "$JobName" | Select-Object -first 1).jobid
+        if (-not $jobId) {throw "Unable to find a job named $JobName within the latest specified build. Did you spell it correctly?"}
+    } else {
+        $jobid = $projectObject.build.jobs[0].jobid
+    }
+
+    $artifacts = Invoke-RestMethod -Method Get -Uri "$apiUrl/buildjobs/$jobId/artifacts" `
+                                   -Headers $headers @proxyArgs
+    $artifacts `
+    | Where-Object { $psCmdlet.ShouldProcess($_.fileName) } `
+    | ForEach-Object {
+
+        $type = $_.type
+
+        $localArtifactPath = $_.fileName -split '/' | ForEach-Object { [Uri]::UnescapeDataString($_) }
+        if ($flat.IsPresent) {
+            $localArtifactPath = ($localArtifactPath | Select-Object -Last 1)
+        } else {
+            $localArtifactPath = $localArtifactPath -join [IO.Path]::DirectorySeparatorChar
+        }
+        $localArtifactPath = Join-Path $path $localArtifactPath
+
+        $artifactUrl = "$apiUrl/buildjobs/$jobId/artifacts/$($_.fileName)"
+        Write-Verbose "Downloading $artifactUrl to $localArtifactPath"
+
+        Invoke-RestMethod -Method Get -Uri $artifactUrl -OutFile $localArtifactPath -Headers $headers @proxyArgs
+
+        New-Object PSObject -Property @{
+            'Source' = $artifactUrl
+            'Type'   = $type
+            'Target' = $localArtifactPath
+        }
+    }
+}
+
+# Initialize Git Directory
+git submodule update --init --recursive
+
+if (${env:APPVEYOR_JOB_NAME} -eq "Build Xirorig_Native Windows") {
+    # Update MSYS2
+    C:\msys64\usr\bin\bash -lc "pacman --noconfirm -Syuu"  # Core update (in case any core packages are outdated)
+    C:\msys64\usr\bin\bash -lc "pacman --noconfirm -Syuu"  # Normal update
+} elseif (${env:APPVEYOR_JOB_NAME} -eq "Build Xirorig_Native Linux") {
+    # Install dependencies
+    sudo apt-get update -y
+    sudo apt-get install curl zip unzip tar p7zip-full cmake ninja-build build-essential pkg-config gcc-10 gcc-10-arm-linux-gnueabihf gcc-10-aarch64-linux-gnu g++-10 g++-10-arm-linux-gnueabihf g++-10-aarch64-linux-gnu -y
+
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 10
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 20
+
+    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 10
+    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-10 20
+
+    sudo update-alternatives --install /usr/bin/arm-linux-gnueabihf-gcc arm-linux-gnueabihf-gcc /usr/bin/arm-linux-gnueabihf-gcc-10 20
+    sudo update-alternatives --install /usr/bin/arm-linux-gnueabihf-g++ arm-linux-gnueabihf-g++ /usr/bin/arm-linux-gnueabihf-g++-10 20
+
+    sudo update-alternatives --install /usr/bin/aarch64-linux-gnu-gcc aarch64-linux-gnu-gcc /usr/bin/aarch64-linux-gnu-gcc-10 20
+    sudo update-alternatives --install /usr/bin/aarch64-linux-gnu-g++ aarch64-linux-gnu-g++ /usr/bin/aarch64-linux-gnu-g++-10 20
+} elseif (${env:APPVEYOR_JOB_NAME} -eq "Build Xirorig_Native MacOS") {
+    # Install dependencies
+    brew update
+    brew install ninja
+    brew install p7zip
+} elseif (${env:APPVEYOR_JOB_NAME} -eq "Build Xirorig Windows") {
+    # Install dotnet enviroment
+    Invoke-WebRequest "https://dot.net/v1/dotnet-install.ps1" -OutFile "${env:APPVEYOR_BUILD_FOLDER}/../dotnet-install.ps1"
+    Set-Location ..
+    ./dotnet-install.ps1 -Channel 6.0 -Quality GA -InstallDir "${env:APPVEYOR_BUILD_FOLDER}/../dotnet"
+
+    # Create Directories
+    New-Item "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out" -ItemType Directory -Force
+
+    # Download Xirorig Native Files
+    Get-AppVeyorArtifacts ${env:APPVEYOR_ACCOUNT_NAME} ${env:APPVEYOR_PROJECT_NAME} -Token ${env:APPVEYOR_TOKEN_KEY} -JobName "Build Xirorig_Native Windows" -DownloadDirectory "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out"
+    7z x "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/${env:XIRORIG_NATIVE_WINDOWS_ARTIFACT_NAME}"
+} elseif (${env:APPVEYOR_JOB_NAME} -eq "Build Xirorig Linux") {
+    # Install dependencies
+    sudo apt-get update -y
+    sudo apt-get install p7zip-full -y
+
+    # Install dotnet enviroment
+    Invoke-WebRequest "https://dot.net/v1/dotnet-install.sh" -OutFile "${env:APPVEYOR_BUILD_FOLDER}/../dotnet-install.sh"
+    Set-Location ..
+    ./dotnet-install.sh -Channel 6.0 -Quality GA -InstallDir "${env:APPVEYOR_BUILD_FOLDER}/../dotnet"
+
+    # Create Directories
+    New-Item "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out" -ItemType Directory -Force
+
+    # Download Xirorig Native Files
+    Get-AppVeyorArtifacts ${env:APPVEYOR_ACCOUNT_NAME} ${env:APPVEYOR_PROJECT_NAME} -Token ${env:APPVEYOR_TOKEN_KEY} -JobName "Build Xirorig_Native Linux" -DownloadDirectory "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out"
+    7z x "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/${env:XIRORIG_NATIVE_LINUX_ARTIFACT_NAME}"
+} elseif (${env:APPVEYOR_JOB_NAME} -eq "Build Xirorig MacOS") {
+    # Install dependencies
+    brew update
+    brew install p7zip
+
+    # Install dotnet enviroment
+    Invoke-WebRequest "https://dot.net/v1/dotnet-install.sh" -OutFile "${env:APPVEYOR_BUILD_FOLDER}/../dotnet-install.sh"
+    Set-Location ..
+    ./dotnet-install.sh -Channel 6.0 -Quality GA -InstallDir "${env:APPVEYOR_BUILD_FOLDER}/../dotnet"
+
+    # Create Directories
+    New-Item "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out" -ItemType Directory -Force
+
+    # Download Xirorig Native Files
+    Get-AppVeyorArtifacts ${env:APPVEYOR_ACCOUNT_NAME} ${env:APPVEYOR_PROJECT_NAME} -Token ${env:APPVEYOR_TOKEN_KEY} -JobName "Build Xirorig_Native MacOS" -DownloadDirectory "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out"
+    7z x "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/out/${env:XIRORIG_NATIVE_MACOS_ARTIFACT_NAME}"
+}
