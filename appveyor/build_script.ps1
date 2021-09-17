42e1cad359a9044401daa95c2f44d09cd4d9153c
Yong Jian Ming <jianming1993@gmail.com>
2021-09-18 00:56:09 +0800

    Update

--- appveyor/build_script.ps1
+++ appveyor/build_script.ps1
@@ -13,54 +13,54 @@ switch -Exact (${env:APPVEYOR_JOB_NAME})
         $MSYS_PROJECT_ROOT = $MSYS_PROJECT_ROOT.Replace('C:\', '/C/')
         $MSYS_PROJECT_ROOT = $MSYS_PROJECT_ROOT.Replace('\', '/')
 
-        C:\msys64\usr\bin\bash -lc "cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=`"$MSYS_PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_INSTALL_ROOT}/x64-windows`" -DVCPKG_TARGET_TRIPLET=x64-mingw-static -S `"$MSYS_PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}`" -B `"$MSYS_PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/x64-windows`" && ninja -C `"$MSYS_PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/x64-windows`" install"
+        Invoke-Expression "C:\msys64\usr\bin\bash -lc `"cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=`"$MSYS_PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_INSTALL_ROOT}/x64-windows`" -DVCPKG_TARGET_TRIPLET=x64-mingw-static -S `"$MSYS_PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}`" -B `"$MSYS_PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/x64-windows`" && ninja -C `"$MSYS_PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/x64-windows`" install`""
 
         # Build Xirorig Native x86
         $env:MSYSTEM = 'MINGW32'
 
-        C:\msys64\usr\bin\bash -lc "cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=`"$MSYS_PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_INSTALL_ROOT}/x86-windows`" -DVCPKG_TARGET_TRIPLET=x86-mingw-static -S `"$MSYS_PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}`" -B `"$MSYS_PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/x86-windows`" && ninja -C `"$MSYS_PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/x86-windows`" install"
+        Invoke-Expression "C:\msys64\usr\bin\bash -lc `"cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=`"$MSYS_PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_INSTALL_ROOT}/x86-windows`" -DVCPKG_TARGET_TRIPLET=x86-mingw-static -S `"$MSYS_PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}`" -B `"$MSYS_PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/x86-windows`" && ninja -C `"$MSYS_PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/x86-windows`" install`""
 
         # Build Xirorig Native ARM32
-        cmd /c "call `"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsamd64_arm.bat`" && cmake -G `"Visual Studio 16 2019`" -A ARM -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=`"$PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_INSTALL_ROOT}/ARM32-windows`" -DVCPKG_TARGET_TRIPLET=arm-windows-static -DEXCLUDE_CPU_INFO_DEPENDENCY=true -S `"$PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}`" -B `"$PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/ARM32-windows`" && msbuild.exe `"${env:APPVEYOR_BUILD_FOLDER}\${env:XIRORIG_NATIVE_ROOT}\${env:XIRORIG_NATIVE_OUTPUT_ROOT}\${env:XIRORIG_NATIVE_BUILD_ROOT}\ARM32-windows\INSTALL.vcxproj`" -p:Configuration=Release"
+        Invoke-Expression "cmd /c `"call `"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsamd64_arm.bat`" && cmake -G `"Visual Studio 16 2019`" -A ARM -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=`"$PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_INSTALL_ROOT}/ARM32-windows`" -DVCPKG_TARGET_TRIPLET=arm-windows-static -DEXCLUDE_CPU_INFO_DEPENDENCY=true -S `"$PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}`" -B `"$PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/ARM32-windows`" && msbuild.exe `"${env:APPVEYOR_BUILD_FOLDER}\${env:XIRORIG_NATIVE_ROOT}\${env:XIRORIG_NATIVE_OUTPUT_ROOT}\${env:XIRORIG_NATIVE_BUILD_ROOT}\ARM32-windows\INSTALL.vcxproj`" -p:Configuration=Release`""
 
         # Build Xirorig Native ARM64
-        cmd /c "call `"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsamd64_arm64.bat`" && cmake -G `"Visual Studio 16 2019`" -A ARM64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=`"$PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_INSTALL_ROOT}/ARM64-windows`" -DVCPKG_TARGET_TRIPLET=arm64-windows-static -DEXCLUDE_CPU_INFO_DEPENDENCY=true -S `"$PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}`" -B `"$PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/ARM64-windows`" && msbuild.exe `"${env:APPVEYOR_BUILD_FOLDER}\${env:XIRORIG_NATIVE_ROOT}\${env:XIRORIG_NATIVE_OUTPUT_ROOT}\${env:XIRORIG_NATIVE_BUILD_ROOT}\ARM64-windows\INSTALL.vcxproj`" -p:Configuration=Release"
+        Invoke-Expression "cmd /c `"call `"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsamd64_arm64.bat`" && cmake -G `"Visual Studio 16 2019`" -A ARM64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=`"$PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_INSTALL_ROOT}/ARM64-windows`" -DVCPKG_TARGET_TRIPLET=arm64-windows-static -DEXCLUDE_CPU_INFO_DEPENDENCY=true -S `"$PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}`" -B `"$PROJECT_ROOT/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/ARM64-windows`" && msbuild.exe `"${env:APPVEYOR_BUILD_FOLDER}\${env:XIRORIG_NATIVE_ROOT}\${env:XIRORIG_NATIVE_OUTPUT_ROOT}\${env:XIRORIG_NATIVE_BUILD_ROOT}\ARM64-windows\INSTALL.vcxproj`" -p:Configuration=Release`""
 
         Set-Location "${env:APPVEYOR_BUILD_FOLDER}\${env:XIRORIG_NATIVE_ROOT}\${env:XIRORIG_NATIVE_OUTPUT_ROOT}\${env:XIRORIG_NATIVE_INSTALL_ROOT}"
 
-        7z a ${env:XIRORIG_NATIVE_ARTIFACT_NAME} *
+        Invoke-Expression "7z a ${env:XIRORIG_NATIVE_ARTIFACT_NAME} *"
         break
     }
 
     "Build Xirorig_Native Linux"
     {
         # Build Xirorig Native x64
-        cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_INSTALL_ROOT}/x64-linux" -DVCPKG_TARGET_TRIPLET=x64-linux -S "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}" -B "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/x64-linux"
-        ninja -C "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/x64-linux" install
+        Invoke-Expression "cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=`"${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_INSTALL_ROOT}/x64-linux`" -DVCPKG_TARGET_TRIPLET=x64-linux -S `"${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}`" -B `"${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/x64-linux`""
+        Invoke-Expression "ninja -C `"${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/x64-linux`" install"
 
         # Build Xirorig Native ARM32
-        cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_INSTALL_ROOT}/ARM32-linux" -DVCPKG_TARGET_TRIPLET=arm-linux -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE="${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/cmake/raspberrypi-arm.cmake" -S "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}" -B "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/ARM32-linux"
-        ninja -C "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/ARM32-linux" install
+        Invoke-Expression "cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=`"${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_INSTALL_ROOT}/ARM32-linux`" -DVCPKG_TARGET_TRIPLET=arm-linux -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=`"${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/cmake/raspberrypi-arm.cmake`" -S `"${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}`" -B `"${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/ARM32-linux`""
+        Invoke-Expression "ninja -C `"${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/ARM32-linux`" install"
 
         # Build Xirorig Native ARM64
-        cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_INSTALL_ROOT}/ARM64-linux" -DVCPKG_TARGET_TRIPLET=arm64-linux -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE="${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/cmake/raspberrypi-arm64.cmake" -S "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}" -B "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/ARM64-linux"
-        ninja -C "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/ARM64-linux" install
+        Invoke-Expression "cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=`"${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_INSTALL_ROOT}/ARM64-linux`" -DVCPKG_TARGET_TRIPLET=arm64-linux -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=`"${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/cmake/raspberrypi-arm64.cmake`" -S `"${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}`" -B `"${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/ARM64-linux`""
+        Invoke-Expression "ninja -C `"${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/ARM64-linux`" install"
 
         Set-Location "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_INSTALL_ROOT}"
 
-        7z a ${env:XIRORIG_NATIVE_ARTIFACT_NAME} *
+        Invoke-Expression "7z a ${env:XIRORIG_NATIVE_ARTIFACT_NAME} *"
         break
     }
 
     "Build Xirorig_Native MacOS"
     {
         # Build Xirorig Native x64
-        cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_INSTALL_ROOT}/x64-osx" -DVCPKG_TARGET_TRIPLET=x64-osx -S "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}" -B "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/x64-osx"
-        ninja -C "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/x64-osx" install
+        Invoke-Expression "cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=`"${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_INSTALL_ROOT}/x64-osx`" -DVCPKG_TARGET_TRIPLET=x64-osx -S `"${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}`" -B `"${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/x64-osx`""
+        Invoke-Expression "ninja -C `"${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_BUILD_ROOT}/x64-osx`" install"
 
         Set-Location "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_NATIVE_ROOT}/${env:XIRORIG_NATIVE_OUTPUT_ROOT}/${env:XIRORIG_NATIVE_INSTALL_ROOT}"
 
-        7z a ${env:XIRORIG_NATIVE_ARTIFACT_NAME} *
+        Invoke-Expression "7z a ${env:XIRORIG_NATIVE_ARTIFACT_NAME} *"
         break
     }
 
@@ -73,12 +73,12 @@ switch -Exact (${env:APPVEYOR_JOB_NAME})
         Invoke-Expression "$dotnet publish -p:PublishProfile=arm-windows.pubxml -p:Platform=ARM32 -c Release_windows"
         Invoke-Expression "$dotnet publish -p:PublishProfile=arm64-windows.pubxml -p:Platform=ARM64 -c Release_windows"
 
-        Set-Location "${env:APPVEYOR_BUILD_FOLDER}\Xirorig\publish"
+        Set-Location "${env:APPVEYOR_BUILD_FOLDER}\${env:XIRORIG_ROOT}\${env:XIRORIG_PUBLISH_ROOT}"
 
-        7z a x64-windows.zip x64-windows -mx=9
-        7z a x86-windows.zip x86-windows -mx=9
-        7z a arm-windows.zip arm-windows -mx=9
-        7z a arm64-windows.zip arm64-windows -mx=9
+        Invoke-Expression "7z a x64-windows.zip x64-windows -mx=9"
+        Invoke-Expression "7z a x86-windows.zip x86-windows -mx=9"
+        Invoke-Expression "7z a arm-windows.zip arm-windows -mx=9"
+        Invoke-Expression "7z a arm64-windows.zip arm64-windows -mx=9"
         break
     }
 
@@ -90,16 +90,16 @@ switch -Exact (${env:APPVEYOR_JOB_NAME})
         Invoke-Expression "$dotnet publish -p:PublishProfile=arm-linux.pubxml -p:Platform=ARM32 -c Release_linux"
         Invoke-Expression "$dotnet publish -p:PublishProfile=arm64-linux.pubxml -p:Platform=ARM64 -c Release_linux"
 
-        Set-Location "${env:APPVEYOR_BUILD_FOLDER}/Xirorig/publish"
+        Set-Location "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_ROOT}/${env:XIRORIG_PUBLISH_ROOT}"
 
-        7z a x64-linux.tar x64-linux
-        7z a x64-linux.tar.gz x64-linux.tar -mx=9
+        Invoke-Expression "7z a x64-linux.tar x64-linux"
+        Invoke-Expression "7z a x64-linux.tar.gz x64-linux.tar -mx=9"
 
-        7z a arm-linux.tar arm-linux
-        7z a arm-linux.tar.gz arm-linux.tar -mx=9
+        Invoke-Expression "7z a arm-linux.tar arm-linux"
+        Invoke-Expression "7z a arm-linux.tar.gz arm-linux.tar -mx=9"
 
-        7z a arm64-linux.tar arm64-linux
-        7z a arm64-linux.tar.gz arm64-linux.tar -mx=9
+        Invoke-Expression "7z a arm64-linux.tar arm64-linux"
+        Invoke-Expression "7z a arm64-linux.tar.gz arm64-linux.tar -mx=9"
         break
     }
 
@@ -109,10 +109,10 @@ switch -Exact (${env:APPVEYOR_JOB_NAME})
 
         Invoke-Expression "$dotnet publish -p:PublishProfile=x64-osx.pubxml -p:Platform=x64 -c Release_osx"
 
-        Set-Location "${env:APPVEYOR_BUILD_FOLDER}/Xirorig/publish"
+        Set-Location "${env:APPVEYOR_BUILD_FOLDER}/${env:XIRORIG_ROOT}/${env:XIRORIG_PUBLISH_ROOT}"
 
-        7z a x64-osx.tar x64-osx
-        7z a x64-osx.tar.gz x64-osx.tar -mx=9
+        Invoke-Expression "7z a x64-osx.tar x64-osx"
+        Invoke-Expression "7z a x64-osx.tar.gz x64-osx.tar -mx=9"
         break
     }
 }
