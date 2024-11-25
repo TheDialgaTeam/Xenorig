a68796093b83b94945662237776a74c8804e94b1
Yong Jian Ming <jianming1993@gmail.com>
2024-11-25 21:52:56 +0800

    Keep

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include "Logger/AnsiEscapeCodeConstants.h"
+#include "Logger/Logger.h"
+#include "Utility/Console.h"
+#include "Utility/CpuInformation.h"
+
+#include <version.h>
+
+int main(int argv, char *argc[])
+{
+    const Xenorig::Logger::Logger logger;
+    const Xenorig::Utility::CpuInformation cpuInformation;
+
+    Xenorig::Utility::Console::SetTitle("Test");
+
+    logger.GetConsole(true)->info(" {}* {}{:<12} {}{}/{} {}{}/{}{}",
+                                   Xenorig::Logger::AnsiEscapeCodeConstants::GreenForegroundColor,
+                                   Xenorig::Logger::AnsiEscapeCodeConstants::WhiteForegroundColor,
+                                   "About",
+                                   Xenorig::Logger::AnsiEscapeCodeConstants::CyanForegroundColor,
+                                   PROJECT_NAME,
+                                   PROJECT_VERSION,
+                                   Xenorig::Logger::AnsiEscapeCodeConstants::DarkGrayForegroundColor,
+                                   COMPILER_ID,
+                                   COMPILER_VERSION,
+                                   Xenorig::Logger::AnsiEscapeCodeConstants::Reset);
+
+    logger.GetConsole(true)->info(" {}* {}{:<12} {}{}{}",
+        Xenorig::Logger::AnsiEscapeCodeConstants::GreenForegroundColor,
+        Xenorig::Logger::AnsiEscapeCodeConstants::WhiteForegroundColor,
+        "CPU",
+        Xenorig::Logger::AnsiEscapeCodeConstants::DarkGrayForegroundColor,
+        cpuInformation.GetProcessorName(),
+        Xenorig::Logger::AnsiEscapeCodeConstants::Reset);
+
+    std::cin.get();
+
+    return 0;
+}
